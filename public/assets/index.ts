982ff5f4cdfb99cc0f8431add3156ec42d133f04
rahulsd380 <rahulsd380@gmail.com>
2025-11-08 22:52:40 +0600

    Added hero image and icons

---
+++ public/assets/index.ts
@@ -0,0 +1,25 @@
+import logo from "./images/logo.webp";
+import heroImg from "./images/hero-img2.webp";
+import heroBg from "./images/hero-bg.webp";
+import circle from "./images/circle.webp";
+import globeCircle from "./images/globe-circle.png";
+import happyClients from "./images/happy-clients.webp";
+import projectCompleted from "./images/project-completed.webp";
+
+// Icons
+import topRightArrow from "./icons/top-right-arrow.svg";
+
+export const IMAGES = {
+  logo,
+  heroImg,
+  heroBg,
+  circle,
+  globeCircle,
+  happyClients,
+  projectCompleted,
+};
+
+
+export const ICONS = {
+  topRightArrow,
+};
