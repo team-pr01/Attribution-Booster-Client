8d5a09b8c7d5fa6240265b62066091c6e0c6645e
rahulsd380 <rahulsd380@gmail.com>
2025-12-08 21:12:02 +0600

    Changed base url

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -10,8 +10,8 @@ import type { RootState } from "../store";
 import { setUser } from "../features/Auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api/v1",
-  // baseUrl: "https://attribution-booster-server.vercel.app/api/v1",
+  // baseUrl: "http://localhost:5000/api/v1",
+  baseUrl: "https://attribution-booster-server.vercel.app/api/v1",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
